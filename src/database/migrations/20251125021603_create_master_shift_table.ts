907c5a2ad92e9cdaad6923317d4032d423714666
titanaprilian <titanaprilian73@gmail.com>
2025-11-25 10:49:37 +0700

    fix: migration database

--- src/database/migrations/20251125021603_create_master_shift_table.ts
+++ src/database/migrations/20251125021603_create_master_shift_table.ts
@@ -15,4 +15,6 @@ export async function up(knex: Knex): Promise<void> {
   });
 }
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("master_shifts");
+}
