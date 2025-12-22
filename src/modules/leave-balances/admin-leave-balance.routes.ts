c3da745a4d8c30d91d3e86c0cfa6fe095f8db04c
Afigo Azus Zakkyfriza <134657953+afigoazus@users.noreply.github.com>
2025-12-22 11:03:57 +0700

    feat: implement feature-based permission system and middleware (#71)

--- src/modules/leave-balances/admin-leave-balance.routes.ts
+++ src/modules/leave-balances/admin-leave-balance.routes.ts
@@ -9,15 +9,54 @@ import {
   updateLeaveBalances,
 } from "./leave-balance.controller.js";
 import { authMiddleware } from "@common/middleware/authMiddleware.js";
+import { FEATURES, PERMISSIONS } from "@common/constants/general.js";
+import { checkPermission } from "@common/middleware/permissionMiddleware.js";
 
 const router = Router();
 router.use(authMiddleware);
 
-router.get("/", fetchAllLeaveBalances);
-router.post("/", createLeaveBalances);
-router.post("/bulk", createBulkLeaveBalances);
-router.put("/:id", updateLeaveBalances);
-router.delete("/bulk", destroyBulkLeaveBalances);
-router.delete("/:id", destroyLeaveBalances);
+const LEAVE_BALANCE_FEATURE = FEATURES.LEAVE_BALANCE_MANAGEMENT;
+
+// get all leave balances
+router.get(
+  "/",
+  checkPermission(LEAVE_BALANCE_FEATURE, PERMISSIONS.READ),
+  fetchAllLeaveBalances
+);
+
+// post leave balance
+router.post(
+  "/",
+  checkPermission(LEAVE_BALANCE_FEATURE, PERMISSIONS.CREATE),
+  createLeaveBalances
+);
+
+// post bulk leave balances
+router.post(
+  "/bulk",
+  checkPermission(LEAVE_BALANCE_FEATURE, PERMISSIONS.CREATE),
+  createBulkLeaveBalances
+);
+
+// put leave balance by id
+router.put(
+  "/:id",
+  checkPermission(LEAVE_BALANCE_FEATURE, PERMISSIONS.UPDATE),
+  updateLeaveBalances
+);
+
+// delete bulk leave balances
+router.delete(
+  "/bulk",
+  checkPermission(LEAVE_BALANCE_FEATURE, PERMISSIONS.DELETE),
+  destroyBulkLeaveBalances
+);
+
+// delete leave balance by id
+router.delete(
+  "/:id",
+  checkPermission(LEAVE_BALANCE_FEATURE, PERMISSIONS.DELETE),
+  destroyLeaveBalances
+);
 
 export default router;
